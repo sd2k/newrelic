4a33ce8c142250987303af5b353caead0b8eb87b
Ben Sully <ben.sully@yougov.com>
2019-05-17 11:27:02 +0100

    Remove custom config from examples

--- examples/external-segment.rs
+++ examples/external-segment.rs
@@ -1,12 +1,8 @@
 use std::{env, thread, time::Duration};
 
-use env_logger::init;
-use newrelic::{App, ExternalParamsBuilder, NewRelicConfig};
+use newrelic::{App, ExternalParamsBuilder};
 
 fn main() {
-    init();
-    NewRelicConfig::default().init().unwrap();
-
     let license_key =
         env::var("NEW_RELIC_LICENSE_KEY").unwrap_or_else(|_| "example-license-key".to_string());
     let app = App::new("my app", &license_key).expect("Could not create app");
